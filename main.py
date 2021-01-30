2a59de14e4150eb502a289b6f02f8fa406adc9a2
cmodog <53931809+cmodog@users.noreply.github.com>
2021-01-30 14:00:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ from bs4 import BeautifulSoup
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 
+
 HEADER_GET = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Linux; Android 11; Mi 10 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36/lenovoofficialapp/16112154380982287_10181446134/newversion/versioncode-124/"
+}
+
+HEADER_COUNT = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
 }
 
 def login() -> (requests.session, str):       #登录过程
@@ -31,12 +36,13 @@ def login() -> (requests.session, str):       #登录过程
 
 def getContinuousDays(session):
     url = "https://club.lenovo.com.cn/signlist/"
-    c = session.get(url,headers=HEADER_GET)
+    c = session.get(url,headers=HEADER_COUNT)
     soup = BeautifulSoup(c.text,"html.parser")
     cc = soup.select("body > div.signInMiddleWrapper > div > div.signInTimeInfo > div.signInTimeInfoMiddle > p.signInTimeMiddleBtn")
     cc = cc[0].get_text()
-    day = cc[5:6]
-    return day
+    if cc == " 已签到 ":
+        return 1
+    return cc[5:6]
 
 
 def signin(session):
